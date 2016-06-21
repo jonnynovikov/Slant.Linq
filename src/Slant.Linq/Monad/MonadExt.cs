331c7f49323402f653bec6a1525660dcef2896af
Ivan Novikov <novikov@amiforus.com>
2016-06-21 21:38:44 +0300

    Moved to Slant repo

---
+++ src/Slant.Linq/Monad/MonadExt.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Monad
+{
+    public static class MonadExt
+    {
+        /// <summary>
+        /// Returns the value if this is a Just, otherwise the other value is returned, if this is a Nothing
+        /// </summary>
+        /// <param name="self"></param>
+        /// <param name="supplier"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static T GetOrElse<T>(this Option<T> self, Func<T> supplier)
+        {
+            if (supplier == null) throw new ArgumentNullException("supplier");
+            var resT = self();
+            if (resT != null && resT.HasValue)
+            {
+                return resT.Value;
+            }
+            return supplier();
+        }
+
+        /// <summary>
+        /// Returns the value if this is a Just case, otherwise throws an exception.
+        /// </summary>
+        /// <param name="self"></param>
+        /// <param name="exceptionSupplier"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="TException"></typeparam>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static T GetOrElseThrow<T, TException>(this Option<T> self, Func<TException> exceptionSupplier) where TException : Exception
+        {
+            if (exceptionSupplier == null) throw new ArgumentNullException("exceptionSupplier");
+            var resT = self();
+            if (resT != null && resT.HasValue)
+            {
+                return resT.Value;
+            }
+            throw exceptionSupplier();
+        }
+        
+    }
+}
